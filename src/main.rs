21d65d95fa025caff865c85e1c1def9e0f81a6b9
Mark Cornick <mcornick@mcornick.com>
2021-02-10 16:14:35 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+// Copyright 2021 Mark Cornick <mcornick@mcornick.com>
+//
+// Permission to use, copy, modify, and/or distribute this software for
+// any purpose with or without fee is hereby granted, provided that the
+// above copyright notice and this permission notice appear in all
+// copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+// WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+// AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+// DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA
+// OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+// TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+// PERFORMANCE OF THIS SOFTWARE.
+
+use rand::Rng;
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let length;
+    if args.len() > 1 {
+        length = args[1].parse().unwrap_or(16);
+    } else {
+        length = 16;
+    };
+    if length < 1 {
+        panic!("length must be an integer greater than 0")
+    };
+
+    let alphabet = [
+        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+        "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+        "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1",
+        "2", "3", "4", "5", "6", "7", "8", "9",
+    ];
+    let mut password = vec![""; length];
+    let mut rng = rand::thread_rng();
+
+    for i in 0..length {
+        let r = rng.gen_range(0..alphabet.len());
+        password[i] = alphabet[r];
+    }
+    println!("{}", password.join(""));
+}
