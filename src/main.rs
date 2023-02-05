fae398482846b9da19816d886b24a9d4327d4597
Mark Cornick <mcornick@mcornick.com>
2023-02-05 00:25:26 -0500

    fix needless_range_loop, finally

--- src/main.rs
+++ src/main.rs
@@ -37,10 +37,9 @@ fn main() {
     let mut password = vec![""; length];
     let mut rng = rand::thread_rng();
 
-    #[allow(clippy::needless_range_loop)]
-    for i in 0..length {
+    (0..length).for_each(|i| {
         let r = rng.gen_range(0..alphabet.len());
         password[i] = alphabet[r];
-    }
+    });
     println!("{}", password.join(""));
 }
